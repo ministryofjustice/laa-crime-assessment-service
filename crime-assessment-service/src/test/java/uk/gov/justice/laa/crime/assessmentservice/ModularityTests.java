c414581758f0cd5c226435c6d61e261c7fd84e53
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2025-05-14 13:50:49 +0100

    test: Add tests for modularity verification
    These tests ensure that:
    - The application meets modularity restrictions as implemented by default in Spring Modulith
    - It creates documentation for these modules

---
+++ crime-assessment-service/src/test/java/uk/gov/justice/laa/crime/assessmentservice/ModularityTests.java
@@ -0,0 +1,19 @@
+package uk.gov.justice.laa.crime.assessmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModularityTests {
+    ApplicationModules modules = ApplicationModules.of(AssessmentServiceApplication.class);
+
+    @Test
+    void verifiesModularStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void createModuleDocumentation() {
+        new Documenter(modules).writeDocumentation();
+    }
+}
