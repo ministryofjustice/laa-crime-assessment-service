6cbf099cfeb7a93b29aef8606b3eb224b19becc6
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2025-05-15 10:52:42 +0100

    feat: Add the ConfigurationPropertiesScan annotation to the main Application class

--- crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
+++ crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
@@ -2,8 +2,10 @@ package uk.gov.justice.laa.crime.assessmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AssessmentServiceApplication {
 
     public static void main(String[] args) {
