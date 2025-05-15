b22e924e3a8835ca100c590f42514f5893c1c65b
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2025-05-15 10:54:39 +0100

    feat: Add Slf4j and RequiredArgsConstructor annotations to controllers

--- crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/iojappeal/IojAppealController.java
+++ crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/iojappeal/IojAppealController.java
@@ -1,11 +1,15 @@
 package uk.gov.justice.laa.crime.assessmentservice.iojappeal;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/iojappeal")
 public class IojAppealController {
 
