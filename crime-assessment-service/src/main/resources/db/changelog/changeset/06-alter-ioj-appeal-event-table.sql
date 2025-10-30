8e115f9fac19d883886716e576d4945777a26eb9
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2025-10-30 15:28:07 +0000

    build: Amend field types, names and remove fields
    - Changed the enums to use the ioj_appeal schema rather than public.
    - Removed the "appeal" prefix on fields in the appeals table as it is redundant.
    - Changed appeal_decision to a boolean and renamed to is_passed.
    - Dropped the appeal_status field in ioj_appeal as we won't be using this field going forward.
    - Dropped the event_type field in the ioj_appeal_event table as the only event we have is "Created".

--- crime-assessment-service/src/main/resources/db/changelog/changeset/06-alter-ioj-appeal-event-table.sql
+++ crime-assessment-service/src/main/resources/db/changelog/changeset/06-alter-ioj-appeal-event-table.sql
@@ -2,5 +2,4 @@
 --changeset jhunt:06-alter-ioj-appeal-event-table.sql
 CREATE INDEX idx_appeal_id ON ioj_appeal.ioj_appeal_event(appeal_id);
 
-CREATE TYPE event_type_enum AS ENUM ('CREATED', 'UPDATED', 'STATUS_CHANGED');
-ALTER TABLE ioj_appeal.ioj_appeal_event ALTER COLUMN event_type TYPE event_type_enum USING event_type::event_type_enum;
+ALTER TABLE ioj_appeal.ioj_appeal_event DROP COLUMN event_type;
--- crime-assessment-service/src/main/resources/db/changelog/changeset/08-alter-ioj-appeal-table.sql
+++ crime-assessment-service/src/main/resources/db/changelog/changeset/08-alter-ioj-appeal-table.sql
@@ -2,20 +2,23 @@
 --changeset jhunt:08-alter-ioj-appeal-table.sql
 ALTER TABLE ioj_appeal.ioj_appeal RENAME COLUMN is_latest_appeal_assessment TO is_current;
 
-CREATE TYPE appeal_reason_enum AS ENUM ('NEW', 'PRI', 'JR');    -- Previous Record Incorrect, Judicial Review
-ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN appeal_reason TYPE appeal_reason_enum 
-    USING appeal_reason::appeal_reason_enum;
+ALTER TABLE ioj_appeal.ioj_appeal RENAME COLUMN appeal_reason TO reason;
+CREATE TYPE ioj_appeal.reason_enum AS ENUM ('NEW', 'PRI', 'JR');    -- Previous Record Incorrect, Judicial Review
+ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN reason TYPE ioj_appeal.reason_enum 
+    USING reason::ioj_appeal.reason_enum;
 
-CREATE TYPE appeal_assessor_enum AS ENUM ('CASEWORKER', 'JUDGE');
-ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN appeal_assessor TYPE appeal_assessor_enum 
-    USING appeal_assessor::appeal_assessor_enum;
+ALTER TABLE ioj_appeal.ioj_appeal RENAME COLUMN appeal_assessor TO assessor;
+CREATE TYPE ioj_appeal.assessor_enum AS ENUM ('CASEWORKER', 'JUDGE');
+ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN assessor TYPE ioj_appeal.assessor_enum 
+    USING assessor::ioj_appeal.assessor_enum;
 
-CREATE TYPE appeal_decision_enum AS ENUM ('PASS', 'FAIL');
-ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN appeal_decision TYPE appeal_decision_enum 
-    USING appeal_decision::appeal_decision_enum;
+ALTER TABLE ioj_appeal.ioj_appeal DROP COLUMN appeal_decision;
+ALTER TABLE ioj_appeal.ioj_appeal ADD COLUMN is_passed BOOLEAN;
 
-CREATE TYPE decision_reason_enum AS ENUM ('LOSS_OF_LIBERTY', 'SUSPENDED_SENTENCE', 
+CREATE TYPE ioj_appeal.decision_reason_enum AS ENUM ('LOSS_OF_LIBERTY', 'SUSPENDED_SENTENCE', 
     'LOSS_OF_LIVELIHOOD', 'DAMAGE_TO_REPUTATION', 'QUESTION_OF_LAW', 'UNDERSTAND_PROCEEDINGS', 
     'WITNESS_TRACE', 'SKILL_EXAM', 'INTERESTS_PERSON', 'OTHER');
-ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN decision_reason TYPE decision_reason_enum 
-    USING decision_reason::decision_reason_enum;
+ALTER TABLE ioj_appeal.ioj_appeal ALTER COLUMN decision_reason TYPE ioj_appeal.decision_reason_enum 
+    USING decision_reason::ioj_appeal.decision_reason_enum;
+
+ALTER TABLE ioj_appeal.ioj_appeal DROP COLUMN appeal_status;
