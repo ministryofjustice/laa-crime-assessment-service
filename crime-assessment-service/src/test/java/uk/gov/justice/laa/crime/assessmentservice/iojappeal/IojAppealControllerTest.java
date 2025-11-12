f771b0b7e1d4f9f88aa12ba7b04ac54d3db52b7b
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-11-12 13:15:37 +0000

    Changing stubs to use new common IoJAppeal object. Changing create to take content for creation. Changing tests accordingly.

--- crime-assessment-service/src/test/java/uk/gov/justice/laa/crime/assessmentservice/iojappeal/IojAppealControllerTest.java
+++ crime-assessment-service/src/test/java/uk/gov/justice/laa/crime/assessmentservice/iojappeal/IojAppealControllerTest.java
@@ -2,12 +2,18 @@ package uk.gov.justice.laa.crime.assessmentservice.iojappeal;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import io.swagger.v3.core.util.ObjectMapperFactory;
+import uk.gov.justice.laa.crime.assessmentservice.common.dto.IojAppealDTO;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = IojAppealController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -17,29 +23,34 @@ class IojAppealControllerTest {
     private MockMvc mockMvc;
 
     private static final String IOJAPPEAL_ENDPOINT = "/api/v1/iojappeal";
-    private static final String IOJAPPEAL_ID_ENDPOINT = IOJAPPEAL_ENDPOINT + "/{id}";
+    private static final String IOJAPPEAL_FIND_ENDPOINT = IOJAPPEAL_ENDPOINT + "/{id}";
+    private static final String IOJAPPEAL_CREATE_ENDPOINT = IOJAPPEAL_ENDPOINT + "/";
     private static final String IOJAPPEAL_LEGACY_ID_ENDPOINT = IOJAPPEAL_ENDPOINT + "/lookup-by-legacy-id/{id}";
     private static final Integer TEST_ID = 123;
+    private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.createJsonConverter();
 
     @Test
     void givenEndpointNotImplemented_whenGetByIdEndpointCalled_thenError() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(IOJAPPEAL_ID_ENDPOINT, TEST_ID))
+        mockMvc.perform(MockMvcRequestBuilders.get(IOJAPPEAL_FIND_ENDPOINT, TEST_ID))
                 .andExpect(status().isNotImplemented());
     }
 
     @Test
-    void givenEndpointNotImplemented_whenCreateByIdEndpointCalled_thenError() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(IOJAPPEAL_ID_ENDPOINT, TEST_ID))
+    void givenEndpointNotImplemented_whenCreateEndpointCalled_thenError() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(IOJAPPEAL_CREATE_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_MAPPER.writeValueAsString(
+                                IojAppealDTO.builder().build())))
                 .andExpect(status().isNotImplemented());
     }
 
     @Test
     void givenIncorrectMethod_whenCreateByIdEndpointCalled_thenError() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete(IOJAPPEAL_ID_ENDPOINT, TEST_ID))
+        mockMvc.perform(MockMvcRequestBuilders.delete(IOJAPPEAL_FIND_ENDPOINT, TEST_ID))
                 .andExpect(status().isMethodNotAllowed());
-        mockMvc.perform(MockMvcRequestBuilders.patch(IOJAPPEAL_ID_ENDPOINT, TEST_ID))
+        mockMvc.perform(MockMvcRequestBuilders.patch(IOJAPPEAL_FIND_ENDPOINT, TEST_ID))
                 .andExpect(status().isMethodNotAllowed());
-        mockMvc.perform(MockMvcRequestBuilders.put(IOJAPPEAL_ID_ENDPOINT, TEST_ID))
+        mockMvc.perform(MockMvcRequestBuilders.put(IOJAPPEAL_FIND_ENDPOINT, TEST_ID))
                 .andExpect(status().isMethodNotAllowed());
     }
 
