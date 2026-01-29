26667392f5c59e5d8f4fba882b74817aa026ed88
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2026-01-29 17:10:01 +0000

    Add asynchronous audit event recording mechanism
    Introduced classes to handle audit event recording using Spring's event-driven architecture. Added `AuditEventRecorder`, `AuditEventListener`, and related components to enable asynchronous processing and persistence of audit events, improving system modularity and reliability.

--- crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
+++ crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
@@ -3,7 +3,11 @@ package uk.gov.justice.laa.crime.assessmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AssessmentServiceApplication {
