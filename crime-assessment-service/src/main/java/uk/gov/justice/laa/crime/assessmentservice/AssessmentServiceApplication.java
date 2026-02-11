e3d0a484e44f1d52d1c769d58bdcce4b5aa0c5bf
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2026-02-11 12:16:25 +0000

    Refactor modularity configuration and enhance Modulith support
    Simplified module setup in `ModularityTests` by removing unnecessary predicates. These changes improve modular structure clarity and maintainability.

--- crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
+++ crime-assessment-service/src/main/java/uk/gov/justice/laa/crime/assessmentservice/AssessmentServiceApplication.java
@@ -3,13 +3,13 @@ package uk.gov.justice.laa.crime.assessmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.modulith.Modulithic;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@Modulithic(sharedModules = {"common"})
 public class AssessmentServiceApplication {
 
     public static void main(String[] args) {
--- crime-assessment-service/src/test/java/uk/gov/justice/laa/crime/assessmentservice/ModularityTests.java
+++ crime-assessment-service/src/test/java/uk/gov/justice/laa/crime/assessmentservice/ModularityTests.java
@@ -4,13 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
-import com.tngtech.archunit.core.domain.JavaClass;
-
 class ModularityTests {
-    ApplicationModules modules = ApplicationModules.of(
-                    AssessmentServiceApplication.class,
-                    JavaClass.Predicates.resideInAnyPackage("uk.gov.justice.laa.crime.assessmentservice.common.."))
-            .verify();
+    ApplicationModules modules =
+            ApplicationModules.of(AssessmentServiceApplication.class).verify();
 
     @Test
     void verifiesModularStructure() {
